2f275e7d119cc6768096ab20167a3e666d07b8b1
Bianca Fu <0509biancafu@gmail.com>
2024-03-19 00:09:03 -0600

    added logo and user location for future reference, removed previous style

--- amplify/backend/function/processPayment/src/index.js
+++ amplify/backend/function/processPayment/src/index.js
@@ -1,6 +1,6 @@
 const { CognitoIdentityServiceProvider } = require("aws-sdk");
 const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
-const USER_POOL_ID = "<userpool_id>";
+const USER_POOL_ID = "us-west-1_H6mQxEJvI";
 const stripe = require("stripe")("<strip_private_key>");
 
 const getUserEmail = async (event) => {
